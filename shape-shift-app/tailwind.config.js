ae5dc80cd9164bc75750acfe993974ed0542b93c
agrawasu <agrawasu@mail.uc.edu>
2024-08-04 08:13:16 -0400

    set up project

--- shape-shift-app/tailwind.config.js
+++ shape-shift-app/tailwind.config.js
@@ -1,18 +1,62 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{js,jsx}',
+    './components/**/*.{js,jsx}',
+    './app/**/*.{js,jsx}',
+    './src/**/*.{js,jsx}',
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "15px",
+    },
+    screens: {
+      "sm": "640px",
+      "md": "768px",
+      "lg": "960px",
+      "xl": "1200px",
+    },
+    fontFamily: {
+      primary: "var(--font-jetbrainsMono)",
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        matcha: {
+          a: "#f1ebe1",
+          b: "#c0cfb2",
+          c: "#8ba888",
+          d: "#44624a",
+        },
+        soft: {
+          a: "#f9e8ce",
+          b: "#bda28d",
+          c: "#d9dbe7",
+          d: "#769898",
+        },
+      },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+      rotate: {
+        360: "360deg",
+        540: "520deg",
+        720: "720deg",
       },
     },
   },
-  plugins: [],
-};
+  plugins: [require("tailwindcss-animate")],
+}
